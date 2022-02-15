0bb7bbaf7d577309ac281832db334f2865adf3d2
Michael Tamirie <michaeltamirie@gmail.com>
2022-02-15 17:18:41 +0300

    Fix linter eslint errors

--- modules/display_time.js
+++ modules/display_time.js
@@ -1,7 +1,8 @@
-export const displayTime = () => {
-    const time = document.getElementById('date');
-    const currentDate = luxon.DateTime.local().toLocaleString(
-        luxon.DateTime.DATETIME_FULL,
-    );
-    time.innerHTML = currentDate;
-}
+/* eslint-disable no-undef */
+export default displayTime = () => {
+  const time = document.getElementById('date');
+  const currentDate = luxon.DateTime.local().toLocaleString(
+    luxon.DateTime.DATETIME_FULL,
+  );
+  time.innerHTML = currentDate;
+};
--- modules/nav_bar.js
+++ modules/nav_bar.js
@@ -2,17 +2,17 @@ const listClass = document.getElementById('List');
 const addNewClass = document.getElementById('Add_new');
 const contactClass = document.getElementById('Contact');
 export const toList = () => {
-    listClass.classList.remove('none');
-    addNewClass.classList.add('none');
-    contactClass.classList.add('none');
-}
+  listClass.classList.remove('none');
+  addNewClass.classList.add('none');
+  contactClass.classList.add('none');
+};
 export const toAddNew = () => {
-    listClass.classList.add('none');
-    addNewClass.classList.remove('none');
-    contactClass.classList.add('none');
-}
+  listClass.classList.add('none');
+  addNewClass.classList.remove('none');
+  contactClass.classList.add('none');
+};
 export const toContact = () => {
-    listClass.classList.add('none');
-    addNewClass.classList.add('none');
-    contactClass.classList.remove('none');
-}
+  listClass.classList.add('none');
+  addNewClass.classList.add('none');
+  contactClass.classList.remove('none');
+};
